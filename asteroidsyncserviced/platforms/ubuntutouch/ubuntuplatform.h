214724690c1512ac22b117127f26b68238ccf8af
Stefan Weng <stefan.weng@gmx.net>
2019-10-01 21:14:24 +0200

    listen on dbus for notifications and send them to the watch

--- asteroidsyncserviced/platforms/ubuntutouch/ubuntuplatform.h
+++ asteroidsyncserviced/platforms/ubuntutouch/ubuntuplatform.h
@@ -12,22 +12,25 @@ class UbuntuPlatform : public QObject, public QDBusContext
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.freedesktop.Notifications")
+    Q_PROPERTY(QDBusInterface* interface READ interface)
 
 public:
     UbuntuPlatform(WatchesManager *wm, QObject *parent = 0);
     ~UbuntuPlatform();
+    QDBusInterface* interface() const;
 
 public slots:
-    void onTimeChanged();
-
+    uint Notify(const QString &app_name, uint replaces_id, const QString &app_icon, const QString &summary, const QString &body, const QStringList &actions, const QVariantHash &hints, int expire_timeout);
     void updateMusicStatus();
     void updateMusicTitle();
     void updateMusicAlbum();
     void updateMusicArtist();
 
 private:
-
-    TimeService *m_timeService;
+    QString encodeIcon(QString appName) const;
+    QString encodeAppName(QString appName) const;
+    
+    QDBusInterface *m_iface;
     MediaService *m_mediaService;
     NotificationService *m_notificationService;
 };
