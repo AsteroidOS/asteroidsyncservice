b67f54f4053c14a73c81ee555d1fb0d9995dd640
Stefan Weng <stefan.weng@gmx.net>
2020-03-22 12:37:58 +0100

    Add media control service. Update title, album and artist on the watch and read cmd like play, pause, next and previous.

--- asteroidsyncserviced/platforms/ubuntutouch/ubuntuplatform.h
+++ asteroidsyncserviced/platforms/ubuntutouch/ubuntuplatform.h
@@ -21,20 +21,39 @@ public:
 
 public slots:
     uint Notify(const QString &app_name, uint replaces_id, const QString &app_icon, const QString &summary, const QString &body, const QStringList &actions, const QVariantHash &hints, int expire_timeout);
-    void NotificationServiceReady();
-    void updateMusicStatus();
-    void updateMusicTitle();
-    void updateMusicAlbum();
-    void updateMusicArtist();
+    void onNotificationServiceReady();
+    void onDisconnected();
+
+private slots:
+    void onMediaServiceReady();
+    void onMprisPropertiesChanged(const QString &interface, const QVariantMap &changedProps, const QStringList &invalidatedProps);
+    void onPreviousMusicTitle();
+    void onNextMusicTitle();
+    void onPlayMusicTitle();
+    void onPauseMusicTitle();
 
 private:
+    QDBusInterface *m_iface;
+
     QString encodeIcon(QString appName) const;
     QString encodeAppName(QString appName) const;
-    
-    QDBusInterface *m_iface;
-    MediaService *m_mediaService;
     NotificationService *m_notificationService;
     bool m_notificationServiceReady = false;
+
+    void setupMusicService();
+    void fetchPropertyAsync(const QString &propertyName);
+    void propertyChanged(const QString &propertyName, const QVariant &value);
+    void sendMusicControlCommand(QString method);
+    void setArtist(QString artist);
+    void setTitle(QString title);
+    void setAlbum(QString album);
+    void setPlaying(QString playingState);
+    MediaService *m_mediaService;
+    QString m_mprisService;
+    QString m_artist;
+    QString m_title;
+    QString m_album;
+    QString m_playingState;
 };
 
 #endif // UBUNTUPLATFORM_H
